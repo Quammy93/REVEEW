b9240731eebfcf73460b64c2b1620e28168d4937
Quammy93 <Quammy93@gmail.com>
2023-10-05 15:11:59 +0000

    REVEEW_APP

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 5000 // the app can use either the hosting port during production 0r 5000 is not available
+const cors = require("cors")
+
+
+
+
+
+// middleware
+
+
+const start = () => {
+    app.listen(port, () => console.log(`Server is running on port ${port}`))
+}
+
+start()
