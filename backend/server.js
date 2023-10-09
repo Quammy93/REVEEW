f2475594bad9de9b996507d75e7551b8f5297e41
Quammy93 <Quammy93@gmail.com>
2023-10-09 16:32:17 +0000

    Reveew update

--- backend/server.js
+++ backend/server.js
@@ -4,25 +4,25 @@ const port = process.env.PORT || 5000; // the app can use either the hosting por
 //const cors = require("cors");
 const notFound = require("./middleware/notFound");
 const errorHandler = require("./middleware/errorHandler");
-
+const connectDb = require("./db/ConnectDb"); 
+require("dotenv").config() //   
 const AuthRoute = require("./routes/auth");
 
 // middleware
 
-//routes
-app.use("/api", AuthController.register);
-app.use("/api", AuthController.login);
+
 
 app.use(notFound);
 app.use(errorHandler);
 
-const start = () => {
-  app.listen(port, () => console.log(`Server is running on port ${port}`));
+const start = async() => {
+await connectDb(process.env.MONGO_URL);         
+    app.listen(port, () => console.log(`Server is running on port ${port}`));
 };
 
 
-
+//routes
 app.use('/api', AuthRoute)
-//app.use('/api', AuthController.login)
+//app.use('/api', AuthRoute)
 
 start()
